ebef192bdfa373307a996ef896070a3f0b530456
ThiagoJO0705 <rm551624@fiap.com.br>
2023-09-17 22:31:56 -0300

    props-types off + variaveis scss

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,12 +10,11 @@ module.exports = {
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh', "react"],
-  rules: {"rules": {
-    "react/prop-types": 0},
+  plugins: ['react-refresh'],
+  rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
-  },
+  },"react/props-types":"off"
 }
