6db89821614ed9b33f7f38c941f70c397f22eef4
Breqsz <rm97974@fiap.com.br>
2023-09-17 23:14:03 -0300

    Criação componente Solucao

---
+++ src/components/Solucao.jsx
@@ -0,0 +1,60 @@
+export default function Solucao(){
+
+
+    
+    return(
+        <>
+            <section className="solucao" id="solucao">
+                <div className="container-solucao">
+                    <h1 id="titulo-solucao">Melhorando o Tráfego de Emergência com Semáforos Inteligentes</h1>
+                    <p id="sub-solucao">Imagine um sistema revolucionário que torna o tráfego de veículos de emergência mais eficiente e seguro. Nossa solução inovadora combina tecnologia de ponta com sensores de proximidade em veículos de resgate e semáforos, garantindo um caminho claro para as ambulâncias chegarem ao seu destino rapidamente.</p>
+                    <div className="containeir-como-funciona-solucao">
+                        <div className="quadrado">
+                            <div id="numero1" className="box-numero">
+                                <h1 className="numero">1</h1>
+                            </div>
+                            <h1 id="titulo-quadrado-1" className="titulo-quadrado">Sensores de Proximidade</h1>
+                            <h2>Instalamos sensores de proximidade em ambulâncias e semáforos.</h2>
+                        </div>
+                        <div className="quadrado">
+                            <div id="box-numero2" className="box-numero">
+                                <h1 className="numero">2</h1>
+                            </div>
+                            <h1 id="titulo-quadrado-2" className="titulo-quadrado">GPS Pré-estabelecido</h1>
+                            <h2>O GPS da ambulância define o caminho, garantindo que apenas os semáforos ao longo da rota sejam afetados, evitando interferências desnecessárias.</h2>
+                        </div>
+                        <div className="quadrado">
+                            <div id="box-numero3" className="box-numero">
+                                <h1 className="numero">3</h1>
+                            </div>
+                            <h1 id="titulo-quadrado-3" className="titulo-quadrado">Ativação do Sinal de Emergência</h1>
+                            <h2>Quando a ambulância se aproxima de um semáforo, o mesmo, se estievr fechado, abre, acompanhado por um som de alerta semelhante à sirene da ambulância.</h2>
+                        </div>
+                        <div className="quadrado">
+                            <div id="box-numero4" className="box-numero">
+                                <h1 className="numero">4</h1>
+                            </div>
+                            <h1 id="titulo-quadrado-4" className="titulo-quadrado">Adaptação Instantânea</h1>
+                            <h2>Se houver uma mudança de rota repentina, nosso sistema ajusta os semáforos de acordo com o novo trajeto da ambulância.</h2>
+                        </div>
+                        <div className="quadrado">
+                            <div id="box-numero5" className="box-numero">
+                                <h1 className="numero">5</h1>
+                            </div>
+                            <h1 id="titulo-quadrado-5" className="titulo-quadrado">Gestão de Cruzamentos</h1>
+                            <h2>Em cruzamentos, o sistema reconhece e fecha os semaforos das vias que cruzam o caminho da ambulância, assim liberando o caminho.</h2>
+                        </div>
+                        <div className="quadrado">
+                            <div id="box-numero6" className="box-numero">
+                                <h1 className="numero">6</h1>
+                            </div>
+                            <h1 id="titulo-quadrado-6" className="titulo-quadrado">Corredores Abertos</h1>
+                            <h2>Os cidadãos são alertados, tanto pelo semáforo quanto pelo som, e abrem caminho para a ambulância passar com segurança.
+                            </h2>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
