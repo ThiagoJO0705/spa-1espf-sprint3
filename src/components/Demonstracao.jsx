11dac57b471280d2f05413de86e1f38603db0628
Rodrigo-Brasileiro <rm98952@fiap.com.br>
2023-09-17 23:18:09 -0300

    adicionado componente demonstracao

---
+++ src/components/Demonstracao.jsx
@@ -0,0 +1,14 @@
+export default function Demonstracao(){
+
+    return(
+        <>
+            <section id="demonstracao" className="demonstracao">
+                <div className="container-demonstracao">
+                    <h1>Revolução no Tráfego Urbano: A Solução para São Paulo</h1>
+                    <h2>Veja como nossa tecnologia está revolucionando o tráfego de emergência em São Paulo em nosso vídeo. Reduzimos atrasos, salvamos vidas e criamos um ambiente de tráfego mais seguro:</h2>
+                    <iframe className="video-yt" src="https://www.youtube.com/embed/XG2X_O7UfWo?si=SZxIQ8N_tRViAdWU" title="Desvendando a Arquitetura dos Semáforos Inteligentes: O futuro das ruas e estradas" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+                </div>
+            </section>
+        </>
+    )
+}
