605619a52d4a711cd37e29d903dcf1a6511f086d
tavares-fiap <RM97877@fiap.com.br>
2023-09-17 23:09:32 -0300

    cria componente home

---
+++ src/components/Home.jsx
@@ -0,0 +1,16 @@
+export default function Home(){
+
+    return(
+        <>
+            <main id="main">
+                <div className="containeir-home">
+                    <h1 id="titulo-home">AmbuZone</h1>
+                    <h2 id="sub-home">Menos Acidentes, Mais Vidas: A História de Sucesso do Nosso Projeto de Semáforo Inteligente</h2>
+                    <div className="btn-projeto">
+                        <button>Conheça nosso projeto →</button>
+                    </div>
+                </div>
+            </main>
+        </>
+    )
+}
